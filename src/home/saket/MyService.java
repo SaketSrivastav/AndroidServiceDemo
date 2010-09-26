3898edef845b1461c0e9ee14fffcf4f6c779b9c0
Saket Srivastav <saketsrivastav@gmail.com>
2010-09-26 10:07:29 +0530

    first commit

---
+++ src/home/saket/MyService.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package home.saket;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * @author saket
+ *
+ */
+public class MyService extends Service {
+	
+	private static final String TAG = "MyService";
+	MediaPlayer player;
+	
+	/* (non-Javadoc)
+	 * @see android.app.Service#onBind(android.content.Intent)
+	 */
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void onCreate() {
+		Toast.makeText(this, "My Service Created", Toast.LENGTH_LONG).show();
+		Log.d(TAG, "onCreate");
+		
+		player = MediaPlayer.create(this, R.raw.allthatjazz);
+		player.setLooping(false); // Set looping
+	}
+
+	@Override
+	public void onDestroy() {
+		Toast.makeText(this, "My Service Stopped", Toast.LENGTH_LONG).show();
+		Log.d(TAG, "onDestroy");
+		player.stop();
+	}
+	
+	@Override
+	public void onStart(Intent intent, int startid) {
+		Toast.makeText(this, "My Service Started", Toast.LENGTH_LONG).show();
+		Log.d(TAG, "onStart");
+		player.start();
+	}
+}
